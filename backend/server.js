f4f1fb1d68d152a7a60534e8e9c97e8d21af0679
Sneha Pal <Sneha1289pal@gmail.com>
2025-08-04 12:47:52 +0530

    Add food model and database config; update server setup

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectToDB = require("./config/db.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//middleware
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Connect to DB
+connectToDB();
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Welcome to the backend server!");
 });
