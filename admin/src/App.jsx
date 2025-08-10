fe8ec5d70d122103761775169b3d54939500a5f0
Sneha-Pal1 <sneha1289pal@gmail.com>
2025-08-10 13:01:43 +0530

    refactor: restructure admin assets and add Navbar/Sidebar components

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import sidebar from "./components/sidebar/sidebar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <sidebar />
+      </div>
+    </div>
+  );
 };
 
 export default App;
