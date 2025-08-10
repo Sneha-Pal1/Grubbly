77fef88f67e86920d1b00759927f1c1e68b742fa
Sneha-Pal1 <sneha1289pal@gmail.com>
2025-08-10 22:59:48 +0530

    Admin panel changes

--- admin/src/assets/admin_assets/assets.js
+++ admin/src/assets/admin_assets/assets.js
@@ -1,17 +1,21 @@
-import logo from './logo.png'
-import add_icon from './add_icon.png'
-import order_icon from './order_icon.png'
-import profile_image from './profile_image.png'
-import upload_area from './upload_area.png'
-import parcel_icon from './parcel_icon.png'
+// import logo from "./logo.png";
+import add_icon from "./add_icon.png";
+import order_icon from "./order_icon.png";
+import profile_image from "./profile_image.png";
+import upload_area from "./upload_area.png";
+import parcel_icon from "./parcel_icon.png";
+import catlogo from "./catlogo.png";
+import logoo from "./logoo.png";
 
-export const assets ={
-    logo,
-    add_icon,
-    order_icon,
-    profile_image,
-    upload_area,
-    parcel_icon
-}
+export const assets = {
+  //   logo,
+  add_icon,
+  order_icon,
+  profile_image,
+  upload_area,
+  parcel_icon,
+  catlogo,
+  logoo,
+};
 
-export const url = 'http://localhost:4000'
+export const url = "http://localhost:3000";
--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Navbar.css";
+import { assets } from "../../assets/admin_assets/assets";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <img className="logoo" src={assets.logoo} alt="" />
+      <img src={assets.catlogo} alt="" />
+    </div>
+  );
 };
 
 export default Navbar;
