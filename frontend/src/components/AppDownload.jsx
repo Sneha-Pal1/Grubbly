37d230263468f002b90177c8964631c987067b20
Sneha-Pal1 <sneha1289pal@gmail.com>
2025-08-13 14:00:20 +0530

    Fix: Correct input field name typo in LoginPopup and update related components

--- frontend/src/components/AppDownload.jsx
+++ frontend/src/components/AppDownload.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import "./AppDownload.css"
-import play_store from '../frontend_assets/play_store.png'
-import app_store from '../frontend_assets/app_store.png'
+import React from "react";
+import "./AppDownload.css";
+import play_store from "../frontend_assets/play_store.png";
+import app_store from "../frontend_assets/app_store.png";
 
 const AppDownload = () => {
   return (
-    <div className='app-download' id='app-download'>
-        <p>For Better Experience Download <br/> Tomato App</p>
-        <div className="app-download-platforms">
-            <img src={play_store} alt=''/>
-            <img src={app_store} alt=''/>
-
-        </div>
+    <div className="app-download" id="app-download">
+      <p>
+        For Better Experience Download <br /> Grubbly App
+      </p>
+      <div className="app-download-platforms">
+        <img src={play_store} alt="" />
+        <img src={app_store} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AppDownload
+export default AppDownload;
