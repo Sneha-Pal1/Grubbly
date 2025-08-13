818f850c4488aa3e30b215be4b3c8fcfe3bec17c
Sneha-Pal1 <sneha1289pal@gmail.com>
2025-08-13 09:36:54 +0530

    Added register feature using jwt & hashedPassword

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,56 @@ import validator from "validator";
 //login user
 const loginUser = async (req, res) => {};
 
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
 //register user
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+  const { name, password, email } = req.body;
+  try {
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({
+        success: true,
+        message: "user already exists",
+      });
+    }
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: true,
+        message: "Please enter a valid email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    const token = createToken(user._id);
+    res.json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
 
 export { loginUser, registerUser };
