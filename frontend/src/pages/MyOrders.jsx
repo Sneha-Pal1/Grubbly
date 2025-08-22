8bc1bc5ec1188920dad8aec1cb7159be68e1d3b4
Sneha-Pal1 <sneha1289pal@gmail.com>
2025-08-22 16:41:37 +0530

    update myorders

--- frontend/src/pages/MyOrders.jsx
+++ frontend/src/pages/MyOrders.jsx
@@ -3,19 +3,28 @@ import "./MyOrders.css";
 import { useContext } from "react";
 import { StoreContext } from "../context/StoreContext";
 import axios from "axios";
+import parcel_icon from "../frontend_assets/parcel_icon.png";
 
 const MyOrders = () => {
   const { url, token } = useContext(StoreContext);
   const [data, setData] = useState([]);
 
   const fetchOrders = async () => {
-    const response = await axios.post(
-      url + "/api/order/userorders",
-      {},
-      { headers: { token } }
-    );
-    setData(response.data.data);
-    console.log(response.data.data);
+    try {
+      const response = await axios.post(
+        url + "/api/order/userorders",
+        {},
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        setData(response.data.data);
+        console.log(response.data.data);
+      } else {
+        console.error("Failed to fetch orders:", response.data.message);
+      }
+    } catch (error) {
+      console.error("Error fetching orders:", error);
+    }
   };
 
   useEffect(() => {
@@ -24,7 +33,37 @@ const MyOrders = () => {
     }
   }, [token]);
 
-  return <div>MyOrders</div>;
+  return (
+    <div className="my-order">
+      <h2>My Orders</h2>
+      <div className="container">
+        {data.map((order, index) => {
+          return (
+            <div key={index} className="my-orders-order">
+              <img src={parcel_icon} alt="" />
+              <p>
+                {order.items
+                  .map((item, itemIndex) => {
+                    if (itemIndex === order.items.length - 1) {
+                      return item.name + " x " + item.quantity;
+                    } else {
+                      return item.name + " x " + item.quantity + ", ";
+                    }
+                  })
+                  .join("")}
+              </p>
+              <p>${order.amount}</p>
+              <p>Items: {order.items.length}</p>
+              <p>
+                <span>&#x25cf;</span> <b>{order.status}</b>
+              </p>
+              <button>Track Order</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default MyOrders;
