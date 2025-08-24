07e44216625bd14d518dfc1d73f9876331b837dd
Sneha-Pal1 <sneha1289pal@gmail.com>
2025-08-24 12:09:17 +0530

    Fix the header image issue

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import headerImg from "../frontend_assets/header_img.png";
 
 const Header = () => {
   return (
-    <div className='header'>
-        <div className="header-contents">
-            <h2>Order your favourite food here</h2>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings.</p>
-            <button>View Menu</button>
-        </div>
+    <div className="header" style={{ backgroundImage: `url(${headerImg})` }}>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes
+          crafted with the finest ingredients and culinary expertise. Our
+          mission is to satisfy your cravings.
+        </p>
+        <button>View Menu</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
