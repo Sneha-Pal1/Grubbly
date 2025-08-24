d35564ba79907bf58733e6141b61e2843e3eb5b9
Sneha-Pal1 <sneha1289pal@gmail.com>
2025-08-24 09:02:20 +0530

    listed orders for admin panel

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -71,5 +71,21 @@ const userOrders = async (req, res) => {
     });
   }
 };
+//listing orders for admin panel
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({
+      success: true,
+      data: orders,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: false,
+    });
+  }
+};
 
-export { placeOrder, verifyOrder, userOrders };
+export { placeOrder, verifyOrder, userOrders, listOrders };
